75de81aa14a5ba2433dd25b30f42ae99ec67bcdf
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-18 16:29:34 +0400

    add order and orderItem types to models

--- internal/models/models.go
+++ internal/models/models.go
@@ -31,3 +31,19 @@ type Product struct{
 	CreatedAt				time.Time	`json:"created_at"`
 }
 
+type Order struct{
+	ID							int						`json:"id"`
+	UserID					int						`json:"user_id"`
+	Status 					string				`json:"status"`
+	TotalPrice			float64				`json:"total_price"`
+	CreatedAt				time.Time			`json:"created_at"`
+	Items						[]OrderItem		`json:"items,omitempty"`
+}
+
+type OrderItem struct{
+	ID							int						`json:"id"`
+	OrderID					int						`json:"order_id"`
+	ProductID				int						`json:"product_id"`
+	Quantity				int						`json:"quantity"`
+	UnitPrice				float64				`json:"unit_price"`
+}
