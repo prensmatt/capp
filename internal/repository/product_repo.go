6e4935c650654a13a5d82248e00f3edb8106848f
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-22 14:46:35 +0400

    product_repo init, insert and getbyid functions

---
+++ internal/repository/product_repo.go
@@ -0,0 +1,52 @@
+package repository
+
+import(
+	"database/sql"
+	"errors"
+
+	"ecommerce/internal/models"
+)
+
+type ProductRepository struct{
+	DB *sql.DB
+}
+
+func NewProductRepository(db *sql.DB) *ProductRepository{
+	return ProductRepository{DB: db}
+}
+
+func (r *ProductRepository) Insert(p *models.Product) error{
+	query := `INSERT INTO products(name,slug,description,price,stock,category_id,image_url)
+						VALUES($1,$2,$3,$4,$5,$6,$7)
+						RETURNING(id,created_at)
+	`
+
+	err := r.DB.QueryRow(query,p.Name,p.Slug,p.Description,p.Price,p.Stock,p.CategoryID,p.ImageURL).Scan(&p.ID,&p.CreatedAt)
+	return err
+}
+
+func (r *ProductRepository) GetByID(id int) (*models.Product,error){
+	query := `SELECT id,name,slug,description,price,stock,category_id,image_url,created_at
+						FROM products WHERE id=$1
+	`
+	err := r.DB.QueryRow(query, id).Scan(&p.ID,&p.Name,&p.Slug,&p.Description,&p.Price,&p.Stock,&p.CategoryID,&p.ImageURL,&p.CreatedAt)
+	if errors.Is(err,sql.ErrNoRows){
+		return nil, models.ErrNotFound
+	}
+	if err != nil{
+		return nil, err
+	}
+	return &p, nil
+}
+
+func (r *ProductRepository) GetAll(limit, offset int)([]*models.Product,error){
+
+}
+
+func (r *ProductRepository) Update(p *models.Product) error{
+
+}
+
+func (r *ProductRepository) Delete(id int) error{
+
+}
