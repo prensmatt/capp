5db7decc3e46b5c69367de3a1aedeb2358cf637f
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-24 14:20:45 +0400

    add routes, repositories and handlers into main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,57 @@
 package main
 
 import(
-	"fmt"
 	"os"
 	"net/http"
 	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/julienschmidt/httprouter"
+
+	"ecommerce/internal/handlers"
+	"ecommerce/internal/repository"
 )
 
 func main(){
+
+	if err := godotenv.Load(); err != nil{
+		log.Println("no .env file found, relying on real environment variables")
+	}
+
 	port := os.Getenv("PORT")
 	if port == ""{
 		port="8080"
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health",func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintln(w, "OK")
-	})
+	dsn := os.Getenv("DB_URL")
+	db,err := repository.NewDB(dsn)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	productRepo := repository.NewProductRepository(db)
+	orderRepo := repository.NewOrderRepository(db)
+
+	productHandler := handlers.NewProductHandler(productRepo)
+	orderHandler := handlers.NewOrderHandler(orderRepo)
+
+	router := httprouter.New()
+
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
+
+	router.GET("/products", productHandler.GetAllProducts)
+	router.GET("/products/:id", productHandler.GetProduct)
+	router.POST("/products", productHandler.CreateProduct)
+	router.PUT("/products/:id", productHandler.UpdateProduct)
+	router.DELETE("/products/:id", productHandler.DeleteProduct)
+	router.POST("/products/:id/image", productHandler.UploadProductImage)
+
+	router.GET("/orders", orderHandler.GetAllOrders)
+	router.GET("/orders/:id", orderHandler.GetOrder)
+	router.POST("/orders", orderHandler.CreateOrder)
+	router.PATCH("/orders/:id/status", orderHandler.UpdateOrderStatus)
 
 	log.Printf("Server starting on: %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module ecommerce
 
 go 1.25.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.12.3
+)
