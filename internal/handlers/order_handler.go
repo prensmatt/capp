402e40d021c43149938d069956adf75e55c321af
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-24 09:46:33 +0400

    add getAllOrders handler

--- internal/handlers/order_handler.go
+++ internal/handlers/order_handler.go
@@ -57,7 +57,24 @@ func (h *OrderHandler) GetOrder(w http.ResponseWriter, r *http.Request, ps httpr
 }
 
 func (h *OrderHandler) GetAllOrders(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-
+	limit := 10
+	if l := r.URL.Query().Get("limit"); l != ""{
+		if parsed, err := strconv.Atoi(l); err == nil{
+			limit = parsed
+		}
+	}
+	offset := 0
+	if o := r.URL.Query().Get("offset"); o != ""{
+		if parsed, err := strconv.Atoi(o); err == nil{
+			offset = parsed
+		}
+	}
+	orders, err := h.Repo.GetAll(limit,offset)
+	if err != nil{
+		writeError(w, http.StatusInternalServerError, "could not fetch orders")
+		return
+	}
+	writeJSON(w,http.StatusOK,orders)
 }
 
 func (h *OrderHandler) UpdateOrderStatus(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
