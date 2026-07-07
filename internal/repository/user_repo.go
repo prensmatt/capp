b50af937e69e8e262d3ef5f9822eef80bbbfc597
Maleyka Heybatova <6nisan2005@gmail.com>
2026-07-07 14:36:36 +0400

    add GetByEmail to userRepo

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -24,3 +24,18 @@ func(r *UserRepository) Insert(u *models.User) error{
 	return err
 }
 
+func(r *UserRepository) GetByEmail(email string)(*models.User, error){
+	var u models.User
+	query := `SELECT id,email,name,password_hash,role,created_at
+	 FROM users WHERE email=$1`
+	err := r.DB.QueryRow(query,email).Scan(&u.ID,&u.Email,&u.Name,&u.PasswordHash,&u.Role,&u.CreatedAt)
+
+	if errors.Is(err,sql.ErrNoRows){
+		return nil, models.ErrNotFound
+	}
+	if err != nil{
+		return nil, err
+	}
+	return &u, nil
+}
+
