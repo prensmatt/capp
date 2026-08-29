cf1c06074bb011261cb827e89a8a27b429094ea7
Maleyka Heybatova <6nisan2005@gmail.com>
2026-08-29 23:35:40 +0400

    add getCategory endpoint to categories

--- internal/handlers/category_handler.go
+++ internal/handlers/category_handler.go
@@ -23,9 +23,28 @@ func NewCategoryHandler(repo *repository.CategoryHandler) *CategoryHandler{
 func(h *CategoryHandler) GetAll(w http.ResponseWriter,r *http.Request, ps httprouter.Params){
 	categories, err := h.Repo.GetAll()
 	if err != nil{
-		log.Print(err)
+		log.Println(err)
 		writeError(w, http.StatusInternalServerError, "could not fetch categories")
 		return
 	}
 	writeJSON(w, http.StatusOK, categories)
+}
+
+func(h *CategoryHandler) GetCategory(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil{
+		writeError(w, http.StatusBadRequest, "invalid id")
+		return
+	}
+	category, err := h.Repo.GetByID(id)
+	if errors.Is(err, models.ErrNotFound){
+		writeError(w, http.StatusNotFound, "category not found")
+		return
+	}
+	if err != nil{
+		log.Println(err)
+		writeError(w, http.StatusInternalServerError, "could not fetch category")
+		return
+	}
+	writeJSON(w, http.StatusOK, category)
 }
